ed3e7fe43d79afc3fc276b0143eb7b68f7fe3987
Ardahan Aytan <135515876+ardahanaytan@users.noreply.github.com>
2024-10-08 01:24:22 +0300

    arayüz güncellemesi

--- YazLab1-1/Form1.Designer.cs
+++ YazLab1-1/Form1.Designer.cs
@@ -28,37 +28,333 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             textBox1 = new TextBox();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            menuCubuk = new FlowLayoutPanel();
+            panelAnaEkran = new Panel();
+            button2 = new Button();
+            panelArama = new Panel();
+            button7 = new Button();
+            panelTarifOnerme = new Panel();
+            button3 = new Button();
+            panelTarifListesi = new Panel();
+            button4 = new Button();
+            panelTarifEkleme = new Panel();
+            button5 = new Button();
+            panelMalzemeListesi = new Panel();
+            button6 = new Button();
+            panelMalzemeEkle = new Panel();
+            button8 = new Button();
+            menuCubukTimer = new System.Windows.Forms.Timer(components);
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuCubuk.SuspendLayout();
+            panelAnaEkran.SuspendLayout();
+            panelArama.SuspendLayout();
+            panelTarifOnerme.SuspendLayout();
+            panelTarifListesi.SuspendLayout();
+            panelTarifEkleme.SuspendLayout();
+            panelMalzemeListesi.SuspendLayout();
+            panelMalzemeEkle.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(434, 179);
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(1169, 5);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(37, 25);
             button1.TabIndex = 0;
-            button1.Text = "button1";
             button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(294, 180);
+            textBox1.Location = new Point(995, 73);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(100, 21);
             textBox1.TabIndex = 1;
-            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1218, 35);
+            panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(31, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(30, 28);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(67, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(210, 17);
+            label1.TabIndex = 0;
+            label1.Text = "SEFA YEMEK TARİFİ ÖNERİ SİSTEMİ";
+            // 
+            // menuCubuk
+            // 
+            menuCubuk.BackColor = SystemColors.ActiveCaptionText;
+            menuCubuk.Controls.Add(panelAnaEkran);
+            menuCubuk.Controls.Add(panelArama);
+            menuCubuk.Controls.Add(panelTarifOnerme);
+            menuCubuk.Controls.Add(panelTarifListesi);
+            menuCubuk.Controls.Add(panelTarifEkleme);
+            menuCubuk.Controls.Add(panelMalzemeListesi);
+            menuCubuk.Controls.Add(panelMalzemeEkle);
+            menuCubuk.Dock = DockStyle.Left;
+            menuCubuk.Location = new Point(0, 35);
+            menuCubuk.Name = "menuCubuk";
+            menuCubuk.Padding = new Padding(0, 30, 0, 0);
+            menuCubuk.Size = new Size(202, 624);
+            menuCubuk.TabIndex = 3;
+            // 
+            // panelAnaEkran
+            // 
+            panelAnaEkran.Controls.Add(button2);
+            panelAnaEkran.Location = new Point(3, 33);
+            panelAnaEkran.Name = "panelAnaEkran";
+            panelAnaEkran.Size = new Size(197, 43);
+            panelAnaEkran.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Black;
+            button2.FlatAppearance.BorderColor = Color.IndianRed;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.Location = new Point(-11, -29);
+            button2.Margin = new Padding(0);
+            button2.Name = "button2";
+            button2.Padding = new Padding(15, 0, 0, 0);
+            button2.Size = new Size(224, 99);
+            button2.TabIndex = 0;
+            button2.Text = "          Ana Ekran";
+            button2.TextAlign = ContentAlignment.MiddleLeft;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // panelArama
+            // 
+            panelArama.Controls.Add(button7);
+            panelArama.Location = new Point(3, 82);
+            panelArama.Name = "panelArama";
+            panelArama.Size = new Size(197, 43);
+            panelArama.TabIndex = 9;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Black;
+            button7.FlatAppearance.BorderColor = Color.IndianRed;
+            button7.FlatAppearance.BorderSize = 0;
+            button7.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button7.ForeColor = SystemColors.ControlLightLight;
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.ImageAlign = ContentAlignment.MiddleLeft;
+            button7.Location = new Point(-11, -29);
+            button7.Margin = new Padding(0);
+            button7.Name = "button7";
+            button7.Padding = new Padding(15, 0, 0, 0);
+            button7.Size = new Size(224, 99);
+            button7.TabIndex = 0;
+            button7.Text = "          Arama";
+            button7.TextAlign = ContentAlignment.MiddleLeft;
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // panelTarifOnerme
+            // 
+            panelTarifOnerme.Controls.Add(button3);
+            panelTarifOnerme.Location = new Point(3, 131);
+            panelTarifOnerme.Name = "panelTarifOnerme";
+            panelTarifOnerme.Size = new Size(197, 43);
+            panelTarifOnerme.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Black;
+            button3.FlatAppearance.BorderColor = Color.IndianRed;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ControlLightLight;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.Location = new Point(-11, -29);
+            button3.Margin = new Padding(0);
+            button3.Name = "button3";
+            button3.Padding = new Padding(15, 0, 0, 0);
+            button3.Size = new Size(224, 99);
+            button3.TabIndex = 0;
+            button3.Text = "          Tarif Önerme";
+            button3.TextAlign = ContentAlignment.MiddleLeft;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // panelTarifListesi
+            // 
+            panelTarifListesi.Controls.Add(button4);
+            panelTarifListesi.Location = new Point(3, 180);
+            panelTarifListesi.Name = "panelTarifListesi";
+            panelTarifListesi.Size = new Size(197, 43);
+            panelTarifListesi.TabIndex = 6;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Black;
+            button4.FlatAppearance.BorderColor = Color.IndianRed;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button4.ForeColor = SystemColors.ControlLightLight;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.Location = new Point(-11, -29);
+            button4.Margin = new Padding(0);
+            button4.Name = "button4";
+            button4.Padding = new Padding(15, 0, 0, 0);
+            button4.Size = new Size(224, 99);
+            button4.TabIndex = 0;
+            button4.Text = "          Tarif Listesi";
+            button4.TextAlign = ContentAlignment.MiddleLeft;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // panelTarifEkleme
+            // 
+            panelTarifEkleme.Controls.Add(button5);
+            panelTarifEkleme.Location = new Point(3, 229);
+            panelTarifEkleme.Name = "panelTarifEkleme";
+            panelTarifEkleme.Size = new Size(197, 43);
+            panelTarifEkleme.TabIndex = 7;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Black;
+            button5.FlatAppearance.BorderColor = Color.IndianRed;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button5.ForeColor = SystemColors.ControlLightLight;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.ImageAlign = ContentAlignment.MiddleLeft;
+            button5.Location = new Point(-11, -29);
+            button5.Margin = new Padding(0);
+            button5.Name = "button5";
+            button5.Padding = new Padding(15, 0, 0, 0);
+            button5.Size = new Size(224, 99);
+            button5.TabIndex = 0;
+            button5.Text = "          Tarif Ekleme";
+            button5.TextAlign = ContentAlignment.MiddleLeft;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // panelMalzemeListesi
+            // 
+            panelMalzemeListesi.Controls.Add(button6);
+            panelMalzemeListesi.Location = new Point(3, 278);
+            panelMalzemeListesi.Name = "panelMalzemeListesi";
+            panelMalzemeListesi.Size = new Size(197, 43);
+            panelMalzemeListesi.TabIndex = 8;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Black;
+            button6.FlatAppearance.BorderColor = Color.IndianRed;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button6.ForeColor = SystemColors.ControlLightLight;
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.ImageAlign = ContentAlignment.MiddleLeft;
+            button6.Location = new Point(-11, -29);
+            button6.Margin = new Padding(0);
+            button6.Name = "button6";
+            button6.Padding = new Padding(15, 0, 0, 0);
+            button6.Size = new Size(224, 99);
+            button6.TabIndex = 0;
+            button6.Text = "          Malzeme Listesi";
+            button6.TextAlign = ContentAlignment.MiddleLeft;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // panelMalzemeEkle
+            // 
+            panelMalzemeEkle.Controls.Add(button8);
+            panelMalzemeEkle.Location = new Point(3, 327);
+            panelMalzemeEkle.Name = "panelMalzemeEkle";
+            panelMalzemeEkle.Size = new Size(197, 43);
+            panelMalzemeEkle.TabIndex = 10;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Black;
+            button8.FlatAppearance.BorderColor = Color.IndianRed;
+            button8.FlatAppearance.BorderSize = 0;
+            button8.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            button8.ForeColor = SystemColors.ControlLightLight;
+            button8.Image = (Image)resources.GetObject("button8.Image");
+            button8.ImageAlign = ContentAlignment.MiddleLeft;
+            button8.Location = new Point(-11, -29);
+            button8.Margin = new Padding(0);
+            button8.Name = "button8";
+            button8.Padding = new Padding(15, 0, 0, 0);
+            button8.Size = new Size(224, 99);
+            button8.TabIndex = 0;
+            button8.Text = "          Malzeme Ekle";
+            button8.TextAlign = ContentAlignment.MiddleLeft;
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // menuCubukTimer
+            // 
+            menuCubukTimer.Interval = 10;
+            menuCubukTimer.Tick += menuCubukTimer_Tick;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1218, 659);
+            Controls.Add(menuCubuk);
+            Controls.Add(panel1);
             Controls.Add(textBox1);
-            Controls.Add(button1);
+            Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.None;
+            IsMdiContainer = true;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuCubuk.ResumeLayout(false);
+            panelAnaEkran.ResumeLayout(false);
+            panelArama.ResumeLayout(false);
+            panelTarifOnerme.ResumeLayout(false);
+            panelTarifListesi.ResumeLayout(false);
+            panelTarifEkleme.ResumeLayout(false);
+            panelMalzemeListesi.ResumeLayout(false);
+            panelMalzemeEkle.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -67,5 +363,24 @@
 
         private Button button1;
         private TextBox textBox1;
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private FlowLayoutPanel menuCubuk;
+        private Panel panelAnaEkran;
+        private Button button2;
+        private Panel panelTarifOnerme;
+        private Button button3;
+        private Panel panelTarifListesi;
+        private Button button4;
+        private Panel panelTarifEkleme;
+        private Button button5;
+        private Panel panelMalzemeListesi;
+        private Button button6;
+        private Panel panelArama;
+        private Button button7;
+        private Panel panelMalzemeEkle;
+        private Button button8;
+        private System.Windows.Forms.Timer menuCubukTimer;
     }
 }
--- YazLab1-1/Form1.cs
+++ YazLab1-1/Form1.cs
@@ -13,6 +13,13 @@ namespace YazLab1_1
         MySqlDataAdapter adapter;
         DataTable dt;
 
+        FormArama arama;
+        FormTarifOnerme tarifOnerme;
+        FormTarifListesi tarifListesi;
+        FormTarifEkleme tarifEkleme;
+        FormMalzemeListesi malzemeListesi;
+        FormMalzemeEkleme malzemeEkleme;
+
         public void initDatabase()
         {
             try
@@ -34,7 +41,7 @@ namespace YazLab1_1
 
                 cmd = new MySqlCommand(query1, con);
                 cmd.ExecuteNonQuery();
-                
+
                 //textBox1.Text = "basarili";
 
                 string query2 = @"CREATE TABLE IF NOT EXISTS `yazlab1`.`malzemeler` (
@@ -49,7 +56,7 @@ namespace YazLab1_1
                 cmd = new MySqlCommand(query2, con);
                 cmd.ExecuteNonQuery();
                 //textBox1.Text = "basarili2";
-                
+
 
                 string query3 = @"CREATE TABLE `iliski` (
                                   `MalzemeIDr` char(10) NOT NULL,
@@ -74,7 +81,7 @@ namespace YazLab1_1
             }
             catch (Exception ex)
             {
-                textBox1.Text = "eyvah" + ex.Message ;
+                textBox1.Text = "eyvah" + ex.Message;
                 Console.WriteLine("Tablo oluþturma hatasý: " + ex.Message);
             }
         }
@@ -87,15 +94,174 @@ namespace YazLab1_1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox1.Text = "deneme";
             initDatabase();
+
+
+
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
+
+        bool menuCubukAktifMi = true;
+
+        private void menuCubukTimer_Tick(object sender, EventArgs e)
+        {
+            if (menuCubukAktifMi)
+            {
+                menuCubuk.Width -= 6;
+                if (menuCubuk.Width <= 55)
+                {
+                    menuCubukAktifMi = false;
+                    menuCubukTimer.Stop();
+                }
+            }
+            else
+            {
+                menuCubuk.Width += 6;
+                if (menuCubuk.Width >= 202)
+                {
+                    menuCubukAktifMi = true;
+                    menuCubukTimer.Stop();
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            menuCubukTimer.Start();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (arama == null)
+            {
+                arama = new FormArama();
+                arama.FormClosed += Arama_FormClosed;
+                arama.MdiParent = this;
+                arama.Show();
+            }
+            else
+            {
+                arama.Activate();
+            }
+        }
+
+        private void Arama_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            arama = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (tarifOnerme == null)
+            {
+                tarifOnerme = new FormTarifOnerme();
+                tarifOnerme.FormClosed += TarifOnerme_FormClosed;
+                tarifOnerme.MdiParent = this;
+                tarifOnerme.Dock = DockStyle.Fill;
+                tarifOnerme.Show();
+            }
+            else
+            {
+                tarifOnerme.Activate();
+            }
+        }
+
+        private void TarifOnerme_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            tarifOnerme = null;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (tarifListesi == null)
+            {
+                tarifListesi = new FormTarifListesi();
+                tarifListesi.FormClosed += TarifListesi_FormClosed;
+                tarifListesi.MdiParent = this;
+                tarifListesi.Dock = DockStyle.Fill;
+                tarifListesi.Show();
+            }
+            else
+            {
+                tarifListesi.Activate();
+            }
+        }
+
+        private void TarifListesi_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            tarifListesi = null;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (tarifEkleme == null)
+            {
+                tarifEkleme = new FormTarifEkleme();
+                tarifEkleme.FormClosed += TarifEkleme_FormClosed;
+                tarifEkleme.MdiParent = this;
+                tarifEkleme.Dock = DockStyle.Fill;
+                tarifEkleme.Show();
+            }
+            else
+            {
+                tarifEkleme.Activate();
+            }
+        }
+
+        private void TarifEkleme_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            tarifEkleme = null;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (malzemeListesi == null)
+            {
+                malzemeListesi = new FormMalzemeListesi();
+                malzemeListesi.FormClosed += malzemeListesi_FormClosed;
+                malzemeListesi.MdiParent = this;
+                malzemeListesi.Dock = DockStyle.Fill;
+                malzemeListesi.Show();
+            }
+            else
+            {
+                malzemeListesi.Activate();
+            }
+
+        }
+
+        private void malzemeListesi_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            malzemeListesi = null;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (malzemeEkleme == null)
+            {
+                malzemeEkleme = new FormMalzemeEkleme();
+                malzemeEkleme.FormClosed += malzemeEkleme_FormClosed;
+                malzemeEkleme.MdiParent = this;
+                malzemeEkleme.Dock = DockStyle.Fill;
+                malzemeEkleme.Show();
+            }
+            else
+            {
+                malzemeEkleme.Activate();
+            }
+        }
+
+        private void malzemeEkleme_FormClosed(object? sender, FormClosedEventArgs e)
+        {
+            malzemeEkleme = null;
+        }
     }
+
+
 
 
 
